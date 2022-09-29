5f4e896e45d6a3767c6f11bf5c75e3bd7e7d3381
spypa <spypa@DESKTOP-OGLQFKO>
2022-09-29 10:52:06 +0900

    테스트

--- Final_test00-1/src/main/java/com/project/test/controller/GroupController.java
+++ Final_test00-1/src/main/java/com/project/test/controller/GroupController.java
@@ -20,54 +20,71 @@ public class GroupController {
 	private static final Logger logger = LoggerFactory.getLogger(GroupController.class);
 	private GroupService groupservice;
 	
-	 @Autowired public GroupController(GroupService groupservice) {
-	 this.groupservice = groupservice; }
+	@Autowired 
+	public GroupController(GroupService groupservice) {
+	this.groupservice = groupservice; }
 	 
 	
-	//@RequestMapping(value="/list", method=RequestMethod.GET)
+	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public ModelAndView grouplist(ModelAndView mv) {
 		mv.setViewName("main/grouplist");
 		return mv;
 		
 	}
 	
-//	@RequestMapping(value="/groupuserinfo")
-//	public ModelAndView memberList(@RequestParam(value="page", defaultValue="1",required=false) int page,
-//									@RequestParam(value="limit", defaultValue="3", required=false) int limit,
-//									ModelAndView mv,
-//									@RequestParam(value="search_field", defaultValue="-1", required=false) int index,
-//									@RequestParam(value="search_word", defaultValue="", required=false) String search_word) {
-//		
-//		int listcount=groupservice.getSearchListCount(index, search_word);//총 리스트 수를 받아옴
-//		List<Member> list = groupservice.getSearchList(index, search_word, page, limit);
-//		
-//		int maxpage = (listcount + limit -1)/limit;
-//		int startpage = ((page-1)/10) *10 +1;
-//		int endpage = startpage +10-1;
-//		
-//		if(endpage>maxpage)
-//			endpage=maxpage;
-//		
-//		mv.setViewName("group/groupuserinfo");
-//		mv.addObject("page",page);
-//		mv.addObject("maxpage",maxpage);
-//		mv.addObject("startpage",startpage);
-//		mv.addObject("endpage",endpage);
-//		mv.addObject("listcount",listcount);
-//		mv.addObject("memberlist",list);
-//		mv.addObject("search_field",index);
-//		mv.addObject("search_word",search_word);
+	@RequestMapping(value="/groupuserinfo")
+	public ModelAndView memberList(@RequestParam(value="page", defaultValue="1",required=false) int page,
+									@RequestParam(value="limit", defaultValue="3", required=false) int limit,
+									ModelAndView mv,
+									@RequestParam(value="search_field", defaultValue="-1", required=false) int index,
+									@RequestParam(value="search_word", defaultValue="", required=false) String search_word) {
+		
+		int listcount=groupservice.getSearchListCount(index, search_word);//총 리스트 수를 받아옴
+		List<Member> list = groupservice.getSearchList(index, search_word, page, limit);
+		
+		int maxpage = (listcount + limit -1)/limit;
+		int startpage = ((page-1)/10) *10 +1;
+		int endpage = startpage +10-1;
+		
+		if(endpage>maxpage)
+			endpage=maxpage;
+		
+		mv.setViewName("group/groupuserinfo");
+		mv.addObject("page",page);
+		mv.addObject("maxpage",maxpage);
+		mv.addObject("startpage",startpage);
+		mv.addObject("endpage",endpage);
+		mv.addObject("listcount",listcount);
+		mv.addObject("memberlist",list);
+		mv.addObject("search_field",index);
+		mv.addObject("search_word",search_word);
+		return mv;
+	}
+	
+//	@RequestMapping(value="/list", method=RequestMethod.GET)
+//	public ModelAndView grouplist(ModelAndView mv) {
+//		mv.setViewName("main/grouplist");
 //		return mv;
+//		
 //	}
 	
-	@RequestMapping(value="/groupjoinagree")
-	public String test() {
-		return "/group/groupjoinagree";
+	@RequestMapping(value="/groupjoinagree", method =RequestMethod.GET)
+	public ModelAndView groupjoinagree(ModelAndView mv) {
+		mv.setViewName("group/groupjoinagree");
+		return mv;
 	}
 	
-	@RequestMapping(value="/groupuserinfo")
-	public String test2() {
-		return "/group/groupuserinfo";
+	@RequestMapping(value="/groupuserinfo", method =RequestMethod.GET)
+	public ModelAndView groupuserinfo(ModelAndView mv) {
+		mv.setViewName("group/groupuserinfo");
+		return mv;
 	}
+	
+	@RequestMapping(value="/groupDisband", method =RequestMethod.GET)
+	public ModelAndView groupDisband(ModelAndView mv) {
+		mv.setViewName("group/groupDisband");
+		return mv;
+	}
+
 
 }
--- Final_test00-1/src/main/java/com/project/test/domain/Member.java
+++ Final_test00-1/src/main/java/com/project/test/domain/Member.java
@@ -1,103 +1,104 @@
 package com.project.test.domain;
 //관리자 회원 조회용 DTO
-	public class Member {
-		private String id;
-		
-		//member_joinForm.jsp에서 비밀번호 name속성 값을 확인해주세요
-		private String password;
-		private String name;
-		private int age;
-		private String gender;
-		private String email;
+   public class Member {
+      private String id;
+      
+      //member_joinForm.jsp에서 비밀번호 name속성 값을 확인해주세요
+      private String password;
+      private String name;
+      private int age;
+      private String gender;
+      private String email;
 
-		private String auth="ROLE_MEMBER";
-		
-		public String getAuth() {
-			return auth;
+      private String auth="ROLE_MEMBER";
+      
+      public String getAuth() {
+         return auth;
+      }
+      private String joindate;
+      private int rnum;
+      private String group_name;
+      private String cnt;
+      private String catename;
+      private String userid;
+      
+      public String getCatename() {
+         return catename;
+      }
 
-		private String joindate;
-		private int rnum;
-		private String group_name;
-		private String cnt;
-		private String catename;
-		
-		public String getCatename() {
-			return catename;
-		}
+      public void setCatename(String catename) {
+         this.catename = catename;
+      }
 
-		public void setCatename(String catename) {
-			this.catename = catename;
-		}
+      public String getGroup_name() {
+         return group_name;
+      }
 
-		public String getGroup_name() {
-			return group_name;
-		}
+      public void setGroup_name(String group_name) {
+         this.group_name = group_name;
+      }
 
-		public void setGroup_name(String group_name) {
-			this.group_name = group_name;
-		}
+      public String getCnt() {
+         return cnt;
+      }
 
-		public String getCnt() {
-			return cnt;
-		}
+      public void setCnt(String cnt) {
+         this.cnt = cnt;
+      }
 
-		public void setCnt(String cnt) {
-			this.cnt = cnt;
-		}
+      public int getRnum() {
+         return rnum;
+      }
 
-		public int getRnum() {
-			return rnum;
-		}
+      public void setRnum(int rnum) {
+         this.rnum = rnum;
+      }
 
-		public void setRnum(int rnum) {
-			this.rnum = rnum;
-		}
+      public String getUserid() {
+         return getUserid();
+      }
 
-		public String getUserid() {
-			return userid;
-		}
+      public void setUserid(String userid) {
+         this.userid = userid;
 
-		public void setUserid(String userid) {
-			this.userid = userid;
-
-		}
-		public void setAuth(String auth) {
-			this.auth = auth;
-		}
-		public String getId() {
-			return id;
-		}
-		public void setId(String id) {
-			this.id = id;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public int getAge() {
-			return age;
-		}
-		public void setAge(int age) {
-			this.age = age;
-		}
-		public String getGender() {
-			return gender;
-		}
-		public void setGender(String gender) {
-			this.gender = gender;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-	}
+      }
+      public void setAuth(String auth) {
+         this.auth = auth;
+      }
+      public String getId() {
+         return id;
+      }
+      public void setId(String id) {
+         this.id = id;
+      }
+      public String getPassword() {
+         return password;
+      }
+      public void setPassword(String password) {
+         this.password = password;
+      }
+      public String getName() {
+         return name;
+      }
+      public void setName(String name) {
+         this.name = name;
+      }
+      public int getAge() {
+         return age;
+      }
+      public void setAge(int age) {
+         this.age = age;
+      }
+      public String getGender() {
+         return gender;
+      }
+      public void setGender(String gender) {
+         this.gender = gender;
+      }
+      public String getEmail() {
+         return email;
+      }
+      public void setEmail(String email) {
+         this.email = email;
+      }
+   }
